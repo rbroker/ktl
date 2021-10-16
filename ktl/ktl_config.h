6907af5a434f51aa889d6117916db7a01269a425
Richard Broker <rstbroker@gmail.com>
2021-10-16 11:18:25 +0100

    Disable allocation tracking in release build

--- ktl/ktl_config.h
+++ ktl/ktl_config.h
@@ -4,7 +4,9 @@
  * Causes pool allocations to be counted in/out. If allocations mismatch,
  * a warning will be printed when the ktl::unload_runtime is called.
  */
+#if _DEBUG
 #define KTL_TRACK_ALLOCATIONS 1
+#endif
 
 /*
  * Causes debug prints to be serialized behind a FAST_MUTEX, to prevent
